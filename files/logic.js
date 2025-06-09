adbf856cc9508d28b5e4099483d677b1c6dd14f6
pipejosh <cristopher.rosas.a@hotmail.com>
2025-06-09 14:05:07 -0500

    add total price

--- files/logic.js
+++ files/logic.js
@@ -12,12 +12,14 @@ let hamburgers = [
 ];
 
 let desserts = [
-    []
+    [7.50, 0],
+    [5.25, 0],
+    [4.25, 0],
+    [7.50, 0],
+    [7.25, 0]
 ]
 
-
-
-function increase(id)
+function increaseBurger(id)
 {
     var labelId = 'qtnInput' + id;
 
@@ -30,20 +32,71 @@ function increase(id)
     currentLabel.value = hamburgers[id][1];
 }
 
-function decrease(id)
+function decreaseBurger(id)
 {
     var labelId = 'qtnInput' + id;
     var currentLabel = document.getElementById(labelId);
-
+    
     if (hamburgers[id][1] <= 0)
     {
         return
     }
-
-
+    
+    
     hamburgers[id][1] -= 1;
     console.log(hamburgers[id][1]);
     currentLabel.value = hamburgers[id][1];
-    
-        
 }
+
+
+
+function increaseDessert(id)
+{
+    var labelId = 'qtnInputDessert' + id;
+
+    desserts[id][1] += 1;
+
+    console.log(desserts[id][1]);
+
+    var currentLabel = document.getElementById(labelId);
+
+    currentLabel.value = desserts[id][1];
+}
+
+function decreaseDessert(id)
+{
+    var labelId = 'qtnInputDessert' + id;
+    var currentLabel = document.getElementById(labelId);
+
+    if (desserts[id][1] <= 0)
+    {
+        return
+    }
+
+    desserts[id][1] -= 1;
+    console.log(desserts[id][1]);
+    currentLabel.value = desserts[id][1];
+}
+
+function submit()
+{
+    var subtotal = 0;
+
+    for (let i = 0; i < hamburgers.length; i++)
+    {
+        subtotal += (hamburgers[i][0] * hamburgers[i][1]);
+
+    }
+
+    for (let j = 0; j < desserts.length; j++)
+    {
+        subtotal += (desserts[j][0] * desserts[j][1]);
+    }
+
+    var total = subtotal * 1.12;
+
+    console.log(total);
+}
+
+
+
